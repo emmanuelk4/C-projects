16c10b9f3fb96b629c1fe7bf7bf9d2f023a8ce73
Murage Kibicho <emmanuelk4@gmail.com>
2020-06-21 04:03:45 +0300

    Fixed indentation again

--- Networking in C/timeNetwork.c
+++ Networking in C/timeNetwork.c
@@ -1,9 +1,9 @@
 Web server to tell you what time it is
 /*Program:  timeNetwork.c
-*Run: 	   gcc timeNetwork.c -o timeNetwork.o
-*Function: Create server showing my computer's time
-*How to use: After run, open browser http://127.0.0.1:8080
-*Expected output: Local time is:Sun Jun 21 03:58:48 2020
+*Run: 	   	gcc timeNetwork.c -o timeNetwork.o
+*Function: 	Create server showing my computer's time
+*Use: 		Run, open browser http://127.0.0.1:8080
+*Output: 	Local time is:Sun Jun 21 03:58:48 2020
 */
 
 #include <sys/types.h>
@@ -38,8 +38,8 @@ int main () {
 	printf("Creating socket...\n");
 	SOCKET socket_listen;
 	socket_listen = socket(bind_address->ai_family,
-						   bind_address->ai_socktype,
-						   bind_address->ai_protocol);
+		bind_address->ai_socktype,
+		bind_address->ai_protocol);
 
 	if(!ISVALIDSOCKET(socket_listen)) {
 		fprintf(stderr, "socket failed. %d\n", GETSOCKETERRNO());
@@ -48,69 +48,69 @@ int main () {
 
 	printf("Binding socket to local addresses...\n");
 	if(bind(socket_listen,
-			bind_address->ai_addr, bind_address->ai_addrlen)) {
+		bind_address->ai_addr, bind_address->ai_addrlen)) {
 		fprintf(stderr, "bin failed. %d\n", GETSOCKETERRNO());
-		return 1;
-	}
-	freeaddrinfo(bind_address);
+	return 1;
+}
+freeaddrinfo(bind_address);
 
-	printf("Listening...\n");
+printf("Listening...\n");
 	//10 is how many connections it can queue
-	if(listen(socket_listen, 10) < 0) {
-		fprintf(stderr, "listen failed. %d\n", GETSOCKETERRNO());
-		return 1;
-	}
+if(listen(socket_listen, 10) < 0) {
+	fprintf(stderr, "listen failed. %d\n", GETSOCKETERRNO());
+	return 1;
+}
 
-	printf("Waiting for connection\n");
-	struct sockaddr_storage client_address;
-	socklen_t client_len = sizeof(client_address);
+printf("Waiting for connection\n");
+struct sockaddr_storage client_address;
+socklen_t client_len = sizeof(client_address);
 
 	//accepts blocks program till connection is made, creates new socket
-	SOCKET socket_client = accept(socket_listen,
-								  (struct sockaddr*) &client_address, &client_len);
+SOCKET socket_client = accept(socket_listen,
+	(struct sockaddr*) &client_address, &client_len);
 
-	if(!ISVALIDSOCKET(socket_client)) {
-		fprintf(stderr, "Accept failed. %d\n", GETSOCKETERRNO());
-		return 1;
-	}
+if(!ISVALIDSOCKET(socket_client)) {
+	fprintf(stderr, "Accept failed. %d\n", GETSOCKETERRNO());
+	return 1;
+}
 
-	printf("Client is connected..\n");
-	char address_buffer[100];
-	getnameinfo((struct sockaddr*) &client_address,
-				 client_len, address_buffer, sizeof(address_buffer), 0,0,
+printf("Client is connected..\n");
+char address_buffer[100];
+getnameinfo((struct sockaddr*) &client_address,
+	client_len, address_buffer, sizeof(address_buffer), 0,0,
 				 NI_NUMERICHOST);//specifies we want to see hostname as ip address
 
-	printf("%s\n", address_buffer);
+printf("%s\n", address_buffer);
 
-	printf("Reading request\n");
-	char request[1024];
-	int bytes_received =recv(socket_client, request, 1024, 0);
-	printf("Received %d bytes\n", bytes_received);
+printf("Reading request\n");
+char request[1024];
+int bytes_received =recv(socket_client, request, 1024, 0);
+printf("Received %d bytes\n", bytes_received);
 
 
-	printf("Sending response...\n"); 	
-	const char *response = "HTTP/1.1 200 OK\r\n"
-						   "COnnection: close\r\n"
-						   "Content-type: text/plain\r\n\r\n"
-						   "Local time is:";
-  	int bytes_sent = send(socket_client, response,strlen(response),0);
-  	printf("Sent %d of %d bytes.\n", bytes_sent, (int) strlen(response));
+printf("Sending response...\n"); 	
+const char *response = "HTTP/1.1 200 OK\r\n"
+"COnnection: close\r\n"
+"Content-type: text/plain\r\n\r\n"
+"Local time is:";
+int bytes_sent = send(socket_client, response,strlen(response),0);
+printf("Sent %d of %d bytes.\n", bytes_sent, (int) strlen(response));
 
   	//get local time frome console and send
-  	time_t timer;
-  	time (&timer);
-  	char *time_msg = ctime(&timer);
-  	bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
-  	printf("Sent %d of %d bytes\n", bytes_sent, (int) strlen(time_msg));
+time_t timer;
+time (&timer);
+char *time_msg = ctime(&timer);
+bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+printf("Sent %d of %d bytes\n", bytes_sent, (int) strlen(time_msg));
 
   	//indicates we've sent all data
-  	printf("CLosing connection...\n");
-  	CLOSESOCKET(socket_client);
+printf("CLosing connection...\n");
+CLOSESOCKET(socket_client);
 
-	printf("CLosing listening socket...\n");
-  	CLOSESOCKET(socket_listen);
+printf("CLosing listening socket...\n");
+CLOSESOCKET(socket_listen);
 
-  	printf("Finished\n");
-  	return 0;
+printf("Finished\n");
+return 0;
 
 }
