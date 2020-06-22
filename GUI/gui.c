cb5d15b00df0b61bec0555f90ebee125750437c8
Murage Kibicho <emmanuelk4@gmail.com>
2020-06-22 22:42:17 +0300

    Main function

--- GUI/gui.c
+++ GUI/gui.c
@@ -1,127 +1,7 @@
-//gcc gui.c -o gui.o `pkg-config --cflags --libs gtk+-3.0`
-#include <gtk/gtk.h>
+//gcc gui.c splashScreen.c functions.c-o gui.o `pkg-config --cflags --libs gtk+-3.0`
+
 #include "splashScreen.h"
-#include <stdio.h>
-#include <stdlib.h>	
-#include <cairo.h>
-#include <errno.h>
-
-//function prototypes
-static void do_drawing(cairo_t *);
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data);
-static void do_drawing(cairo_t *cr);
-static gboolean clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data);
-static gboolean on_window_draw (GtkWidget *widget, GdkEventExpose *event, gpointer data);
-static void on_open_image(GtkButton *button, gpointer user_data);
-gint delete_event(GtkWidget *widget, GdkEvent *event, gpointer data);
-//end of function prototypes
-
-
-/* Global variables for storing mouse coordinates,
-* count is index of arrays, coordx and coordy are x and y coordinates of the mouse
-*/
-struct {
-	int count;
-	double coordx[100];
-	double coordy[100];
-} glob;
-
-/* Function: do_drawing
-*Parameters: cairo_t
-*Use: It sets cairo canvas settings, and draws shapes with a for loop
-*Settings: are commented
-*Note: printf is used during debugging to find mouse click coordinates :)
-*/
-static void do_drawing(cairo_t *cr)
-{
-  cairo_set_source_rgb(cr, 0, 0, 0);//Line colour
-  cairo_set_line_width(cr, 0.5);//Line width
-  cairo_translate(cr, -170, -170);//Shift where line is seen
-
-  //i is starting point, i+1 is next mouse coordinate 
-  int i;
-  for (i = 0; i < glob.count - 1; i++ ) {
-  	cairo_move_to(cr, glob.coordx[i], glob.coordy[i]);
-  	cairo_line_to(cr, glob.coordx[i+1], glob.coordy[i+1]);
-  	printf("from x:%f, y:%f\t to: x:%f, y:%f\n",glob.coordx[i],glob.coordy[i], glob.coordx[i+1], glob.coordy[i+1]);
-  	cairo_stroke(cr);    
-  }
-  //resets array so shape can be drawn again
-  //glob.count = 0;
-}
-
-
-static gboolean on_window_draw (GtkWidget *widget, GdkEventExpose *event, gpointer data)
-{
-
-	GdkWindow *window = gtk_widget_get_window(widget);
-	cairo_region_t *cairo_region = cairo_region_create();
-	GdkDrawingContext *drawing_context = gdk_window_begin_draw_frame(window, cairo_region);
-
-	cairo_t *cr;
-	cr = gdk_drawing_context_get_cairo_context(drawing_context);
-
-	do_drawing(cr);
-       // say: "I'm finished drawing
-	gdk_window_end_draw_frame(window,drawing_context);
-
-
-	cairo_region_destroy (cairo_region);
-    //    return FALSE;
-}
-
-static gboolean clicked(GtkWidget *widget, GdkEventButton *event,
-	gpointer user_data)
-{
-	if (event->button == 1) {
-		glob.coordx[glob.count] = event->x;
-		glob.coordy[glob.count++] = event->y;
-	}
-	if (event->button == 3) {
-		gtk_widget_queue_draw(widget);
-	}
-
-	return TRUE;
-}
-static void on_open_image(GtkButton *button, gpointer user_data)
-{
-	GtkWidget *image = GTK_WIDGET(user_data);
-	GdkPixbuf *pb;
-	GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-	GtkWidget *top_level = gtk_widget_get_toplevel(image);
-	GtkFileFilter *filter = gtk_file_filter_new();
-	GtkWidget *dialog = gtk_file_chooser_dialog_new(("open Image"), GTK_WINDOW(top_level),
-		action,
-		("_Cancel"),
-		GTK_RESPONSE_CANCEL,
-		("_Open"),
-		GTK_RESPONSE_ACCEPT,
-		NULL);
-	gtk_file_filter_add_pixbuf_formats(filter);
-	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
-
-	switch(gtk_dialog_run(GTK_DIALOG(dialog))) 
-	{
-		case GTK_RESPONSE_ACCEPT:
-		{
-			gchar *file_name = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-			pb = gdk_pixbuf_new_from_file(file_name, NULL);
-			pb = gdk_pixbuf_scale_simple(pb,700,700,GDK_INTERP_BILINEAR);
-			gtk_image_set_from_pixbuf(GTK_IMAGE(image), pb);
-			break;
-		}
-		default:
-		break;
-	}
-	gtk_widget_destroy(dialog);
-}
-//Popping up do you want close window
-gint delete_event(GtkWidget *widget, GdkEvent *event, gpointer data) 
-{
-	gtk_main_quit();
-	//if return TRUE will close
-	return FALSE;
-}
+#include "functions.h"
 
 int main (int argc, char *argv[] ) 
 {
@@ -134,9 +14,10 @@ int main (int argc, char *argv[] )
 	GtkWidget *box;
 	GtkWidget *canvas;
 	GtkDrawingArea *drawing_area;
+	struct counter count1;
 
 	//Initalize GTK window
-	glob.count = 0;
+	count1.count = 0;
 	gtk_init(&argc, &argv);
 	
 
@@ -198,8 +79,7 @@ int main (int argc, char *argv[] )
 
 	//Main signal to handle delete_event for window
 	g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
-
-	//show open dialog when open clicked
+//show open dialog when open clicked
 	g_signal_connect(open, "clicked", G_CALLBACK(on_open_image), imagecont);
 	//create drawing area
 	g_signal_connect(drawing_area, "draw", G_CALLBACK(on_window_draw), NULL);
